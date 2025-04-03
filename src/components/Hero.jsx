e41ab49555b1f2bf57293b6d9cd99ecea211ae12
Calcifer077 <maheshnashier14@gmail.com>
2025-04-03 23:12:11 +0530

    Fixed bug in Contact Info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,74 +1,5 @@
-// import { FaGithub, FaLinkedin } from "react-icons/fa";
-
-// function Hero() {
-//   return (
-//     <div
-//       className="flex flex-col lg:flex-row justify-between text-2xl tracking-wide m-10 mx-w-md"
-//       id="about"
-//     >
-//       {/* Left Section with Image */}
-//       <div className="w-1/2 flex justify-center items-center mx-auto lg:ml-26">
-//         <img
-//           src="profile-pic.png"
-//           className="w-[300px] h-[300px] object-cover rounded-full border-2 border-gray-400"
-//           alt="Profile"
-//         />
-//       </div>
-
-//       {/* Right Section with Text */}
-//       <div className="w-full lg:w-1/2 flex flex-col justify-center mx-auto text-center xl:pr-20 pt-10 lg:pt-0">
-//         <div className="text-lg">Hello, I'm</div>
-//         <div className="font-bold text-5xl tracking-wider pt-2">Mahesh</div>
-//         <div className="text-3xl tracking-wider pt-2">FullStack Developer</div>
-
-//         {/* Buttons start here */}
-//         {/* <div className="flex justify-between gap-8 text-lg mt-8">
-//           <button className="border-2 p-2 font-[600] w-50 border-gray-500 rounded-4xl hover:bg-gray-900 hover:text-gray-50 transition-all duration-150 cursor-pointer">
-//             Download Resume
-//           </button>
-//           <button className="border-2 p-2 font-[600] w-50 border-gray-500 rounded-4xl bg-gray-900 text-gray-100 hover:bg-gray-100 hover:text-gray-900 transition-all duration-150 cursor-pointer">
-//             Contact Info
-//           </button>
-//         </div> */}
-
-//         <div className="flex flex-col lg:flex-row justify-between items-center gap-8 text-lg mt-8">
-//           <button className="border w-8/10 h-full md:w-7/10 border-gray-500 text-gray-700 py-2 px-6 rounded-full hover:bg-gray-700 hover:text-gray-100 transition-all duration-300 cursor-pointer">
-//             Download Resume
-//           </button>
-//           <button className="border w-8/10 h-full md:w-7/10 border-gray-500 text-white py-2 px-6 rounded-full bg-gray-900 hover:bg-gray-700 transition-all duration-300 cursor-pointer">
-//             Contact Info
-//           </button>
-//         </div>
-//         {/* Buttons ends here */}
-
-//         {/* Social Links start here */}
-//         <div className="flex justify-center gap-6 mt-8">
-//           <a
-//             href="https://github.com/Calcifer077"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="text-4xl text-gray-900 hover:text-gray-900 transition-colors duration-150"
-//           >
-//             <FaGithub />
-//           </a>
-//           <a
-//             href="https://www.linkedin.com/in/mahesh-nashier-b05691249/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="text-4xl text-gray-900 hover:text-blue-700 transition-colors duration-150"
-//           >
-//             <FaLinkedin />
-//           </a>
-//         </div>
-//         {/* Social Links ends here */}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Hero;
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 function Hero() {
   return (
@@ -121,7 +52,9 @@ function Hero() {
           </button>
 
           <button className="border w-8/10 xl:w-1/2 h-full border-gray-500 text-white py-3 px-6 rounded-full bg-gray-900 hover:bg-gray-700 transition-all duration-300 cursor-pointer">
-            Contact Info
+            <Link to="contact" smooth={true} duration={500}>
+              Contact Info
+            </Link>
           </button>
         </div>
 
